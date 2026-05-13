46a4c64ed5d44ba39783a3dbf0ff4785db066f9d
Wuyang Chen <chenwydj@gmail.com>
2026-05-12 23:34:40 -0700

    include SobolevSpace in index

--- PDE.lean
+++ PDE.lean
@@ -2,3 +2,5 @@ import PDE.Basics.Heat.HeatKernel
 import PDE.Basics.Heat.HeatSolution
 import PDE.Basics.Heat.HeatSolutionProperty
 import PDE.Basics.Heat.HeatMaximumPrinciple
+import PDE.SobolevSpace.Lp_function_spaces
+import PDE.SobolevSpace.weak_derivative
