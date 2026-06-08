57651808d344f4922ab07a0d749697dc42b5d040
weiran-sun <weiransun@gmail.com>
2026-06-07 22:26:48 -0700

    Create Hk_Spaces.lean

---
+++ PDE/SobolevSpace/Hk_Spaces.lean
@@ -0,0 +1,12 @@
+import Mathlib.MeasureTheory.Measure.SeparableMeasure
+import Mathlib.Analysis.InnerProductSpace.Dual
+import Mathlib.Analysis.InnerProductSpace.l2Space
+import Mathlib.Topology.MetricSpace.Sequences
+
+import LeanProjects.Galerkin.Lp_function_spaces
+import LeanProjects.Galerkin.weak_derivative
+import LeanProjects.Galerkin.sobolev
+
+open MeasureTheory
+open ENNReal
+open Filter Topology
